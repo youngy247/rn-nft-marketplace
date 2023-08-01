e7071553405ab138085b954ea937945945d2e31b
Adam Young <adamyoungy678@gmail.com>
2023-08-01 15:05:50 +0100

    feat: Implement CircleButton component for user interactions

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { TouchableOpacity, View, Text, Image } from 'react-native'
 
-export const CircleButton = () => {
+import { COLORS, SIZES, SHADOWS, FONTS } from '../constants'
+
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: 'absolute',
+        borderRadius: SIZES.extraLarge,
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...SHADOWS.light,
+        ...props
+      }}
+      onPress={handlePress}
+    > 
+      <Image
+        source={imgUrl}
+        resizeMode='contain'
+        style={{ width: 24, height: 24 }}
+      />
+
+    </TouchableOpacity>
   )
 }
 
