448772121f7743e7b45c3ec8682016ea30af7340
Adam Young <adamyoungy678@gmail.com>
2023-08-01 13:34:15 +0100

    chore: added constants directory and imports for the Home component

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,8 @@
-import { Text } from 'react-native'
+import { useState } from 'react'
+import { View, SafeArea, FlatList, Text} from 'react-native'
+
+import { COLORS, NFTData } from '../constants' 
+
 
 const Home = () => {
   return (
