a2d3458bb702fbb8a278457783a7be42f471c53a
Adam Young <adamyoungy678@gmail.com>
2023-08-01 13:42:23 +0100

    chore: setup components and exported them

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FocusesStatusBar = () => {
+  return (
+    <View>
+      <Text>FocusesStatusBar</Text>
+    </View>
+  )
+}
+
+export default FocusesStatusBar
