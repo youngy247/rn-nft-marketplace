4f2859e5eb6d8b7eea257d59f74a4134f8713575
Adam Young <adamyoungy678@gmail.com>
2023-08-02 13:37:57 +0100

    feat: added picture of NFT to deatils header with back and like buttons and added unstyled bids

--- screens/Details.js
+++ screens/Details.js
@@ -1,7 +1,35 @@
 import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
-
+import React from 'react'
 import { COLORS, SIZES, FONTS, SHADOWS, assets } from '../constants'
 import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from '../components'
+
+const DetailsHeader = ({ data, navigation }) => (
+  <View style={{
+    width: "100%",
+    height: 373
+  }}>
+    <Image 
+      source={data.image}
+      resizeMode="cover"
+      style={{
+        width: "100%", 
+        height: '100%'
+      }}
+    />
+
+    <CircleButton 
+      imgUrl={assets.left} 
+      handlePress={() => navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 10}
+    />
+    <CircleButton 
+      imgUrl={assets.heart} 
+      right={15}
+      top={StatusBar.currentHeight + 10}
+    />
+  </View>
+)
 
 const Details = ({ route, navigation }) => {
   const { data } = route.params
@@ -29,6 +57,21 @@ const Details = ({ route, navigation }) => {
         }}>
         <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
       </View>
+
+      <FlatList 
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: SIZES.extraLarge * 3
+        }}
+        ListHeaderComponent={() => (
+          <React.Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+          </React.Fragment>
+        )}
+      />
     </SafeAreaView>
   )
 }
