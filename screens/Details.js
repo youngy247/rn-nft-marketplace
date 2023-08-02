acaadd562bedea31613f5ec6bb0a2cc380e4ed88
Adam Young <adamyoungy678@gmail.com>
2023-08-02 14:03:06 +0100

    feat: added current bids to details screen

--- screens/Details.js
+++ screens/Details.js
@@ -74,6 +74,16 @@ const Details = ({ route, navigation }) => {
               padding: SIZES.font,
             }}>
               <DetailsDesc data={data} />
+
+            {data.bids.length > 0 && (
+                <Text style={{
+                  fontSize: SIZES.font,
+                  fontFamily: FONTS.semiBold,
+                  color: COLORS.primary,
+                }}>
+                  Current Bids
+                </Text>
+              )} 
             </View>
           </React.Fragment>
         )}
